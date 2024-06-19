db6155976809e56be9adbc5169256e22c83fcd57
sujeongmoon <msjk9000@gmail.com>
2024-06-19 19:13:59 +0900

    [#5] update: comment 작성 기능 구현

--- src/main/java/com/sparta/dailyswitter/domain/comment/controller/CommentController.java
+++ src/main/java/com/sparta/dailyswitter/domain/comment/controller/CommentController.java
@@ -1,10 +1,19 @@
 package com.sparta.dailyswitter.domain.comment.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sparta.dailyswitter.domain.comment.dto.CommentRequestDto;
+import com.sparta.dailyswitter.domain.comment.dto.CommentResponseDto;
 import com.sparta.dailyswitter.domain.comment.service.CommentService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/api")
@@ -14,4 +23,11 @@ public class CommentController {
 
 	private final CommentService commentService;
 
+	@PostMapping("/{postId}/comments")
+	public ResponseEntity<CommentResponseDto> createComment(@PathVariable Long postId,
+		@RequestBody @Valid CommentRequestDto requestDto,
+		@AuthenticationPrincipal UserDetailsImpl userDetails) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(commentService.createComment
+			(postId, requestDto, userDetails.getUser()));
+	}
 }
--- src/main/java/com/sparta/dailyswitter/domain/comment/service/CommentService.java
+++ src/main/java/com/sparta/dailyswitter/domain/comment/service/CommentService.java
@@ -2,12 +2,33 @@ package com.sparta.dailyswitter.domain.comment.service;
 
 import org.springframework.stereotype.Service;
 
+import com.sparta.dailyswitter.domain.comment.dto.CommentRequestDto;
+import com.sparta.dailyswitter.domain.comment.dto.CommentResponseDto;
+import com.sparta.dailyswitter.domain.comment.entity.Comment;
 import com.sparta.dailyswitter.domain.comment.repository.CommentRepository;
+import com.sparta.dailyswitter.domain.post.entity.Post;
+import com.sparta.dailyswitter.domain.post.service.PostService;
+import com.sparta.dailyswitter.domain.user.entity.User;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
+	private final PostService postService;
 	private final CommentRepository commentRepository;
+
+	public CommentResponseDto createComment(Long postId, CommentRequestDto requestDto, User user) {
+		Post post = postService.findById(postId);
+		Comment comment = Comment.builder()
+			.user(user)
+			.post(post)
+			.requestDto(requestDto)
+			.build();
+
+		commentRepository.save(comment);
+		return CommentResponseDto.builder()
+			.comment(comment)
+			.build();
+	}
 }
