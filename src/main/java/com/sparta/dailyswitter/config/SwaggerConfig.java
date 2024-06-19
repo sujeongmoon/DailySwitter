b6757db59fb0c1bb613f26b46c4cddba2a35f4c7
marlboro09 <taebong8989@gmail.com>
2024-06-19 22:04:25 +0900

    [#4] 🎉 add : Swagger 세팅 완료

--- src/main/java/com/sparta/dailyswitter/config/SwaggerConfig.java
+++ src/main/java/com/sparta/dailyswitter/config/SwaggerConfig.java
@@ -1,33 +1,49 @@
 package com.sparta.dailyswitter.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
-
 	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-			.select()
-			.apis(RequestHandlerSelectors.any())
-			.paths(PathSelectors.any())
-			.build()
-			.apiInfo(apiInfo());
-	}
+	public OpenAPI customOpenAPI() {
+		// 액세스 토큰 보안 스키마 설정
+		SecurityScheme accessTokenScheme = new SecurityScheme()
+			.type(SecurityScheme.Type.HTTP)
+			.scheme("bearer")
+			.bearerFormat("JWT")
+			.in(SecurityScheme.In.HEADER)
+			.name("Authorization");
 
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-			.title("DailySwitter")
-			.description("하루 일상을 공유해봐요")
-			.version("1.0")
-			.build();
+		// 리프레시 토큰 보안 스키마 설정
+		SecurityScheme refreshTokenScheme = new SecurityScheme()
+			.type(SecurityScheme.Type.HTTP)
+			.scheme("bearer")
+			.bearerFormat("JWT")
+			.in(SecurityScheme.In.HEADER)
+			.name("Refresh-Token");
+
+		// 보안 요구 사항 설정
+		SecurityRequirement securityRequirement = new SecurityRequirement()
+			.addList("accessTokenAuth")
+			.addList("refreshTokenAuth");
+
+		return new OpenAPI()
+			.components(new Components()
+				.addSecuritySchemes("accessTokenAuth", accessTokenScheme)
+				.addSecuritySchemes("refreshTokenAuth", refreshTokenScheme))
+			.security(Arrays.asList(securityRequirement))
+			.info(new Info()
+				.title("DailySwitter")
+				.description("하루 일상을 같이 공유 해봐요.")
+				.version("1.0"));
 	}
 }
