ac8bcc6695714825989ffdbdbb32fe89e15334ca
HyeonjinChoi <eaglushyeonjin@gmail.com>
2024-06-19 12:28:38 +0900

    Init commit

---
+++ src/main/java/com/sparta/dailyswitter/DailySwitterApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.dailyswitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailySwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailySwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/dailyswitter/DailySwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.dailyswitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailySwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
