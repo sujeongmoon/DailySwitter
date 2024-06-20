5d23511f75236cb981bdb8ab4c6f43fce0e200f3
HyeonjinChoi <eaglushyeonjin@gmail.com>
2024-06-21 02:39:19 +0900

    [#2] update: Swagger API 문서화 어노테이션 추가

--- src/main/java/com/sparta/dailyswitter/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/dailyswitter/domain/user/controller/UserController.java
@@ -14,9 +14,12 @@ import com.sparta.dailyswitter.domain.user.dto.UserResponseDto;
 import com.sparta.dailyswitter.domain.user.service.UserService;
 import com.sparta.dailyswitter.security.UserDetailsImpl;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "User API", description = "사용자 정보에 대한 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
@@ -24,6 +27,7 @@ public class UserController {
 
 	private final UserService userService;
 
+	@Operation(summary = "Get User", description = "프로필 조회 기능입니다.")
 	@GetMapping
 	public ResponseEntity<UserResponseDto> getUser(
 		@AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -31,6 +35,7 @@ public class UserController {
 		return ResponseEntity.ok().body(userService.getUser(userDetails.getUser().getId()));
 	}
 
+	@Operation(summary = "Update User Info", description = "프로필 기본정보에 대한 수정 기능입니다.")
 	@PutMapping
 	public ResponseEntity<UserResponseDto> updateUserInfo(
 		@AuthenticationPrincipal UserDetailsImpl userDetails,
@@ -39,6 +44,7 @@ public class UserController {
 		return ResponseEntity.ok().body(userService.updateUserInfo(userDetails.getUser().getId(), userInfoRequestDto));
 	}
 
+	@Operation(summary = "Update Password", description = "사용자의 비밀번호 수정 기능입니다.")
 	@PutMapping("/password")
 	public ResponseEntity<UserResponseDto> updatePassword(
 		@AuthenticationPrincipal UserDetailsImpl userDetails,
