fd49dc84005d6a485ec5742952a0e68b4f18a07e
sujeongmoon <msjk9000@gmail.com>
2024-06-24 12:09:00 +0900

    [#17] update : postLike 좋아요 등록, 삭제 기능 구현

--- src/main/java/com/sparta/dailyswitter/domain/like/postlike/controller/PostLikeController.java
+++ src/main/java/com/sparta/dailyswitter/domain/like/postlike/controller/PostLikeController.java
@@ -1,7 +1,16 @@
 package com.sparta.dailyswitter.domain.like.postlike.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sparta.dailyswitter.domain.like.postlike.service.PostLikeService;
+import com.sparta.dailyswitter.security.UserDetailsImpl;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,4 +18,22 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class PostLikeController {
+
+	private final PostLikeService postLikeService;
+
+	@PostMapping("/posts/{postId}/likes")
+	public ResponseEntity<String> createPostLikes(@PathVariable Long postId,
+		@AuthenticationPrincipal UserDetailsImpl userDetails) {
+		postLikeService.createPostLike(postId, userDetails.getUser());
+		return ResponseEntity.status(HttpStatus.OK)
+			.body("게시물 좋아요가 등록되었습니다.");
+	}
+
+	@DeleteMapping("/posts/{postId}/likes")
+	public ResponseEntity<String> deletePostLikes(@PathVariable Long postId,
+		@AuthenticationPrincipal UserDetailsImpl userDetails) {
+		postLikeService.deletePostLike(postId, userDetails.getUser());
+		return ResponseEntity.status(HttpStatus.OK)
+			.body("게시물 좋아요가 삭제되었습니다.");
+	}
 }
--- src/main/java/com/sparta/dailyswitter/domain/like/postlike/service/PostLikeService.java
+++ src/main/java/com/sparta/dailyswitter/domain/like/postlike/service/PostLikeService.java
@@ -1,10 +1,67 @@
 package com.sparta.dailyswitter.domain.like.postlike.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sparta.dailyswitter.common.exception.CustomException;
+import com.sparta.dailyswitter.common.exception.ErrorCode;
+import com.sparta.dailyswitter.domain.like.postlike.entity.PostLike;
+import com.sparta.dailyswitter.domain.like.postlike.entity.PostLikeId;
+import com.sparta.dailyswitter.domain.like.postlike.repository.PostLikeRepository;
+import com.sparta.dailyswitter.domain.post.entity.Post;
+import com.sparta.dailyswitter.domain.post.service.PostService;
+import com.sparta.dailyswitter.domain.user.entity.User;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class PostLikeService {
+
+	private final PostLikeRepository postLikeRepository;
+	private final PostService postService;
+
+	@Transactional
+	public void createPostLike(Long postId, User user) {
+		Post post = postService.findById(postId);
+
+		PostLikeId postLikeId = PostLikeId.builder()
+			.user(user)
+			.post(post)
+			.build();
+
+		if (postLikeRepository.findById(postLikeId).isPresent()) {
+			throw new CustomException(ErrorCode.POST_LIKE_EXIST);
+		}
+
+		postService.checkPostUserFound(post, user);
+
+		PostLike postLike = PostLike.builder()
+			.id(postLikeId)
+			.build();
+
+		postLikeRepository.save(postLike);
+	}
+
+	@Transactional
+	public void deletePostLike(Long postId, User user) {
+		Post post = postService.findById(postId);
+
+		PostLikeId postLikeId = PostLikeId.builder()
+			.user(user)
+			.post(post)
+			.build();
+
+		if (postLikeRepository.findById(postLikeId).isEmpty()) {
+			throw new CustomException(ErrorCode.POST_LIKE_NOT_EXIST);
+		}
+
+		postService.checkPostUserFound(post, user);
+
+		PostLike postLike = PostLike.builder()
+			.id(postLikeId)
+			.build();
+
+		postLikeRepository.delete(postLike);
+	}
 }
